d5b5e73a18cd110831cc5fa4eef52dac119dcc8a
RamPlayzTheEpic <105378964+RamPlayzTheEpic@users.noreply.github.com>
2022-05-11 17:09:41 +0100

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,19 @@
+
+num1 = float(input("Enter no.1: "))
+op = (input("Enter +, -, *, /: "))
+num2 = float(input("Enter no.2: "))
+
+if op == "+":
+    print(num1 + num2)
+
+elif op == "-":
+    print(num1 - num2)
+
+elif op == "*":
+      print(num1 * num2) 
+
+elif op == "/":
+     print(num1 / num2)  
+
+else:
+    print("Invalid Operator!")
